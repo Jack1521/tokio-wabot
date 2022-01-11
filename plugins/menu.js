8b9bb640c825877f57de5fbd701d23e747722f04
Jack <94463439+Jack1521@users.noreply.github.com>
2022-01-11 05:02:05 +0330

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -34,7 +34,7 @@ let tags = {
 const defaultMenu = {
   before: `
 
-🙏🏻 Namaste %name, how can i help you?
+👋🏻 Ohayou %name, how can i help you?
 
 🪵 Left: *%limit Limit*
 🎗️ Role: *%role*
@@ -43,15 +43,15 @@ const defaultMenu = {
 
 〽️ Prefix: *%p*
 📅 Date: *%week, %date*
-💠 Github: github.com/anshulpurbey/tokio-wabot
+💠 Github: https://github.com/Jack1521
 
 👇🏻 All usable commands are listed below 
 
 %readmore`.trimStart(),
   header: '        *━━❰･%category･❱━━*',
-  body: ' 🌠 %cmd %islimit %isPremium',
+  body: ' 🦄 %cmd %islimit %isPremium',
   footer: ' ',
-  after: `🌟 *Hope you're enjoying bot, have a great day* 
+  after: `❤ *Thank you for using jackbot, have a great day* 
 `,
 }
 let handler = async (m, { conn, usedPrefix: _p }) => {
@@ -150,7 +150,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.send2ButtonImg(m.chat, thumb, `🏮 I\'m ${conn.user.name}`, text.trim(), 'owner', '#owner', 'rules', '#rules', m)
+    conn.send2ButtonImg(m.chat, thumb, `🌿 I\'m ${conn.user.name}`, text.trim(), 'owner', '#owner', 'rules', '#rules', m)
   } catch (e) {
     conn.reply(m.chat, 'Sorry, the menu is in error', m)
     throw e
